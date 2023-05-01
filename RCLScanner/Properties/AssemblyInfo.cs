fc82b73bd6b9bf92e539db012900af06553a5038
Werner van der Berg <vdbergwe@gmail.com>
2023-05-01 19:58:15 +0200

    123

--- RCLScanner/Properties/AssemblyInfo.cs
+++ RCLScanner/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RCLScanner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RCL Foods")]
 [assembly: AssemblyProduct("RCLScanner")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
