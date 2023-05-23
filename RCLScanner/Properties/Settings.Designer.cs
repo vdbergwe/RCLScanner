368c2bab63494b6003a5081810f3411bc99aaf04
vdbergwe <vdbergwe@gmail.com>
2023-05-23 09:35:46 +0200

    Fault FInding

--- RCLScanner/Properties/Settings.Designer.cs
+++ RCLScanner/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RCLScanner.Properties
-{
-
-
+namespace RCLScanner.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=rclmlsql01.tsb.co.za;Initial Catalog=zzPODScanDB;User ID=podscanadmin" +
+            ";Password=sO0x7S]2LEd0hn;Multiple Active Result Sets=True;Application Name=Entit" +
+            "yFramework")]
+        public string zzPODScanDBConnectionString {
+            get {
+                return ((string)(this["zzPODScanDBConnectionString"]));
             }
         }
     }
